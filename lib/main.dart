aae447902147564b7a726d1b54d6b36e7906fcd3
UTSAV KUMAR <storm11pegasus@gmail.com>
2023-06-26 11:49:19 +0530

    first

---
+++ lib/main.dart
@@ -0,0 +1,283 @@
+import 'dart:ui';
+import 'dart:io' as io;
+import 'package:flutter/services.dart';
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:camera/camera.dart';
+import 'package:google_mlkit_object_detection/google_mlkit_object_detection.dart';
+
+late List<CameraDescription> cameras;
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  cameras = await availableCameras();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(
+        title: 'screen',
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  dynamic controller;
+  bool isBusy = false;
+  dynamic objectDetector;
+  late Size size;
+
+  @override
+  void initState() {
+    super.initState();
+    initializeCamera();
+  }
+
+  //TODO code to initialize the camera feed
+  initializeCamera() async {
+    //TODO initialize detector
+    final modelPath = await _getModel('assets/ml/mobilenet2.tflite');
+    final options = LocalObjectDetectorOptions(
+      modelPath: modelPath,
+      classifyObjects: true,
+      multipleObjects: true,
+      mode: DetectionMode.stream,
+    );
+    objectDetector = ObjectDetector(options: options);
+
+    //TODO initialize controller
+    controller = CameraController(cameras[0], ResolutionPreset.high);
+    await controller.initialize().then((_) {
+      if (!mounted) {
+        return;
+      }
+      controller.startImageStream((image) => {
+            if (!isBusy)
+              {isBusy = true, img = image, doObjectDetectionOnFrame()}
+          });
+    });
+  }
+
+  //close all resources
+  @override
+  void dispose() {
+    controller?.dispose();
+    objectDetector.close();
+    super.dispose();
+  }
+
+  //TODO object detection on a frame
+  dynamic _scanResults;
+  CameraImage? img;
+
+  //new object model
+  Future<String> _getModel(String assetPath) async {
+    if (io.Platform.isAndroid) {
+      return 'flutter_assets/$assetPath';
+    }
+    final path = '${(await getApplicationSupportDirectory()).path}/$assetPath';
+    await io.Directory(dirname(path)).create(recursive: true);
+    final file = io.File(path);
+    if (!await file.exists()) {
+      final byteData = await rootBundle.load(assetPath);
+      await file.writeAsBytes(byteData.buffer
+          .asUint8List(byteData.offsetInBytes, byteData.lengthInBytes));
+    }
+    return file.path;
+  }
+
+  doObjectDetectionOnFrame() async {
+    var frameImg = getInputImage();
+    List<DetectedObject> objects = await objectDetector.processImage(frameImg);
+    for (DetectedObject detectedObject in objects) {
+      final rect = detectedObject.boundingBox;
+      final trackingId = detectedObject.trackingId;
+
+      for (Label label in detectedObject.labels) {
+        print('${label.text} ${label.confidence}');
+      }
+    }
+    // print("len= ${objects.length}");
+    setState(() {
+      _scanResults = objects;
+      isBusy = false;
+    });
+  }
+
+  InputImage getInputImage() {
+    final WriteBuffer allBytes = WriteBuffer();
+    for (final Plane plane in img!.planes) {
+      allBytes.putUint8List(plane.bytes);
+    }
+    final bytes = allBytes.done().buffer.asUint8List();
+    final Size imageSize = Size(img!.width.toDouble(), img!.height.toDouble());
+    final camera = cameras[0];
+    final imageRotation =
+        InputImageRotationValue.fromRawValue(camera.sensorOrientation);
+    // if (imageRotation == null) return;
+
+    final inputImageFormat =
+        InputImageFormatValue.fromRawValue(img!.format.raw);
+    // if (inputImageFormat == null) return null;
+
+    final planeData = img!.planes.map(
+      (Plane plane) {
+        return InputImagePlaneMetadata(
+          bytesPerRow: plane.bytesPerRow,
+          height: plane.height,
+          width: plane.width,
+        );
+      },
+    ).toList();
+
+    final inputImageData = InputImageData(
+      size: imageSize,
+      imageRotation: imageRotation!,
+      inputImageFormat: inputImageFormat!,
+      planeData: planeData,
+    );
+
+    final inputImage =
+        InputImage.fromBytes(bytes: bytes, inputImageData: inputImageData);
+
+    return inputImage;
+  }
+
+  //Show rectangles around detected objects
+  Widget buildResult() {
+    if (_scanResults == null ||
+        controller == null ||
+        !controller.value.isInitialized) {
+      return const Text('');
+    }
+
+    final Size imageSize = Size(
+      controller.value.previewSize!.height,
+      controller.value.previewSize!.width,
+    );
+    CustomPainter painter = ObjectDetectorPainter(imageSize, _scanResults);
+    return CustomPaint(
+      painter: painter,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    List<Widget> stackChildren = [];
+    size = MediaQuery.of(context).size;
+    if (controller != null) {
+      stackChildren.add(
+        Positioned(
+          top: 0.0,
+          left: 0.0,
+          width: size.width,
+          height: size.height,
+          child: Container(
+            child: (controller.value.isInitialized)
+                ? AspectRatio(
+                    aspectRatio: controller.value.aspectRatio,
+                    child: CameraPreview(controller),
+                  )
+                : Container(),
+          ),
+        ),
+      );
+
+      stackChildren.add(
+        Positioned(
+            top: 0.0,
+            left: 0.0,
+            width: size.width,
+            height: size.height,
+            child: buildResult()),
+      );
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text("Object detector"),
+        backgroundColor: Color.fromARGB(255, 32, 166, 219),
+      ),
+      backgroundColor: Colors.black,
+      body: Container(
+        margin: const EdgeInsets.only(top: 0),
+        color: Colors.black,
+        child: Stack(
+          children: stackChildren,
+        ),
+      ),
+    );
+  }
+}
+
+class ObjectDetectorPainter extends CustomPainter {
+  ObjectDetectorPainter(this.absoluteImageSize, this.objects);
+
+  final Size absoluteImageSize;
+  final List<DetectedObject> objects;
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final double scaleX = size.width / absoluteImageSize.width;
+    final double scaleY = size.height / absoluteImageSize.height;
+
+    final Paint paint = Paint()
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 3.0
+      ..color = Colors.pinkAccent;
+
+    for (DetectedObject detectedObject in objects) {
+      canvas.drawRect(
+        Rect.fromLTRB(
+          detectedObject.boundingBox.left * scaleX,
+          detectedObject.boundingBox.top * scaleY,
+          detectedObject.boundingBox.right * scaleX,
+          detectedObject.boundingBox.bottom * scaleY,
+        ),
+        paint,
+      );
+
+      var list = detectedObject.labels;
+      for (Label label in list) {
+        print("${label.text}   ${label.confidence.toStringAsFixed(2)}");
+        TextSpan span = TextSpan(
+            text: label.text,
+            style: const TextStyle(
+                fontSize: 30, color: Color.fromARGB(255, 255, 255, 255)));
+        TextPainter tp = TextPainter(
+            text: span,
+            textAlign: TextAlign.left,
+            textDirection: TextDirection.ltr);
+        tp.layout();
+        tp.paint(
+            canvas,
+            Offset(detectedObject.boundingBox.left * scaleX,
+                detectedObject.boundingBox.top * scaleY));
+        break;
+      }
+    }
+  }
+
+  @override
+  bool shouldRepaint(ObjectDetectorPainter oldDelegate) {
+    return oldDelegate.absoluteImageSize != absoluteImageSize ||
+        oldDelegate.objects != objects;
+  }
+}
